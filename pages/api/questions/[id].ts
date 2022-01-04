2a28a67571255f0809a39c0500735224b8688510
Pedro Manoel <dev.pedromanoel@pm.me>
2022-01-04 13:04:58 -0300

    chore: use the conversion function

--- pages/api/questions/[id].ts
+++ pages/api/questions/[id].ts
@@ -3,5 +3,5 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import questions from "../questionsDB";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(questions[0]);
+  res.status(200).json(questions[0].toObject());
 }
