425756ae8ec4bfef6b17c717447d5894165965d1
Pedro Manoel <dev.pedromanoel@pm.me>
2022-01-04 13:06:31 -0300

    feat: return a specific data

--- pages/api/questions/[id].ts
+++ pages/api/questions/[id].ts
@@ -3,5 +3,12 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import questions from "../questionsDB";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(questions[0].toObject());
+  const selectedID = +req.query.id;
+  const selectedQuestion = questions.filter(question => question.id === selectedID);
+
+  if(selectedQuestion.length === 1) {
+    res.status(200).json(selectedQuestion[0].toObject());
+  } else {
+    res.status(204).send(res.statusCode);
+  }
 }
