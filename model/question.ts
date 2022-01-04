bebcf49cc1b21da5c1d2af73f3d3cd47d253c357
Pedro Manoel <dev.pedromanoel@pm.me>
2022-01-04 12:56:24 -0300

    chore: create a new get function
    this function is responsible to check if the question was answered

--- model/question.ts
+++ model/question.ts
@@ -1,10 +1,12 @@
+import AnswerModel from "./answer";
+
 export default class QuestionModel {
   #id: number
   #question: string
-  #answers: any[]
+  #answers: AnswerModel[]
   #correct: boolean
 
-  constructor(id: number, question: string, answers: any[], correct: boolean) {
+  constructor(id: number, question: string, answers: AnswerModel[], correct: boolean) {
     this.#id = id;
     this.#question = question;
     this.#answers = answers;
@@ -22,8 +24,15 @@ export default class QuestionModel {
   get answers() {
     return this.#answers;
   }
-  
+
   get correct() {
     return this.#correct;
+  }
+
+  get answered() {
+    for(let answer of this.#answers) {
+      if(answer.revealed) return true;
+    }
+    return false;
   }
 }
