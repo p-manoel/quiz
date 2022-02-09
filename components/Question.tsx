f0db5baf233e9f208522f6b3d953b24eb330845f
Pedro Manoel <dev.pedromanoel@pm.me>
2022-02-09 11:57:03 -0300

    feat: create an Answer component

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,6 +1,7 @@
 import styles from "../styles/Question.module.css";
 import QuestionModel from "../model/question";
 import Utterance from "./Utterance";
+import Answer from "./Answer";
 
 interface QuestionProps {
   value: QuestionModel
@@ -9,9 +10,22 @@ interface QuestionProps {
 export default function Question(props: QuestionProps) {
   const question = props.value;
 
+  function renderAnswers() {
+    return question.answers.map((answer, i) => {
+      return <Answer 
+        key={i}
+        value={answer}
+        index={i}
+        alternative='A'
+        colorAlternative="#F2C866"
+      />
+    })
+  }
+
   return (
     <div className={styles.question}>
       <Utterance text={question.question}/>
+      {renderAnswers()}
     </div>
   )
 }
